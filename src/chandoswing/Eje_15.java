372dfe45f3f29f89df33d4dfe804eaac74d3c6f3
Xoan Araujo <xoanaraujodev@gmail.com>
2023-03-28 13:58:58 +0200

    Eje15

---
+++ src/chandoswing/Eje_15.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Eje_15 extends JFrame {
+    private JLabel label;
+
+    public Eje_15() {
+        super("Eje_15");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        ImageIcon imagenFondo = new ImageIcon("src/chandoswing/image.png");
+
+        JLabel imagenLabel = new JLabel(imagenFondo);
+        imagenLabel.setLayout(new BorderLayout());
+
+        label = new JLabel();
+        imagenLabel.add(label, BorderLayout.CENTER);
+
+        // Creamos el JScrollPane y lo agregamos al JLabel
+        JScrollPane scrollPane = new JScrollPane(imagenLabel);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+
+        // Agregamos el JScrollPane al marco
+        setContentPane(scrollPane);
+
+        setSize(500, 500);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new Eje_15();
+    }
+}
